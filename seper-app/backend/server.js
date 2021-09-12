60b51056f097feda05cb84ef538b913af04c2e4b
Travish <83637066+TravisPDC@users.noreply.github.com>
2021-09-12 13:34:11 +1200

    commit

--- seper-app/backend/server.js
+++ seper-app/backend/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const mongooes = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
 //create express server
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ extended: false }));
@@ -18,6 +18,16 @@ connection.once('open', () => {
     console.log("MongoDB connection established.");
 })
 
+//check if in production
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('seper-app/build'));
+
+    app.get('*', (req, res)=> {
+        res.sendFile(path.resolve(__dirname, 'seper-app', 'build', 'index.html'));
+    });
+}
+
+const PORT = process.env.PORT || 5000;
 //start server
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
