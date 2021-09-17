382e1aaca79b8ef5764f12a8bac6b07a9d5cdabf
Travish <83637066+TravisPDC@users.noreply.github.com>
2021-09-18 10:43:36 +1200

    Revert "Merge pull request #1 from AUT-CISE-Semster2/Travis"
    This reverts commit 4689d75c549b62c990cc775e10f2d0bdbe448665, reversing
    changes made to ae5f86ee94ecb98c6df8609ec4229949a2ab59cb.

--- seper-app/src/pages/SE-Practice.js
+++ seper-app/src/pages/SE-Practice.js
@@ -1,28 +1,13 @@
 import React from "react";
-import articles from "../Data/articles.js";
-import Styles from "../components/tablestyle.js";
-import Table from "../components/evidencetable.js";
-import tablecolumns from "../components/tablecolumns.js";
-
+ 
 const SEPractice = () => {
     return (
       <div>
         <h2>Select SE Practice to get evidence for the claimed benefits</h2>
-        <div class="SE-search">
-          <input type="text" placeholder="Search..." />
-        </div>
-        <div>
-             <Styles>
-                <Table
-                  data={articles}
-                  columns={tablecolumns}
-                  />
-              </Styles>
-        </div>
         <p>There will be a way to select an SE practice from our SEPER repository here</p>
         <p>Also the display table of evidence levels for different claims will show here for the SE practice selected</p>
       </div>
     );
-} 
+}
  
 export default SEPractice;
