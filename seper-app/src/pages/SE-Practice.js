8d3c70d5aec4742940118c3ef420f297032f398b
reyhan2498 <reyhan2498@gmail.com>
2021-09-19 21:16:56 +1200

    search bar

--- seper-app/src/pages/SE-Practice.js
+++ seper-app/src/pages/SE-Practice.js
@@ -1,16 +1,22 @@
+/* eslint-disable eqeqeq */
 import articles from "../Data/articles.js";
 import Styles from "../components/tablestyle.js";
 import Table from "../components/evidencetable.js";
 import tablecolumns from "../components/tablecolumns.js";
 import Dropdown from "../components/Dropdown.js";
 
+
 const SEPractice = () => {
     return (
       <div>
         <h2>Select SE Practice to get evidence for the claimed benefits</h2>
         <div class="SEsearch-bar">
-          <input type="search" placeholder="Search for SE Practice" id="search-input"/>
-        </div>
+          <input 
+            type="search" 
+            placeholder="Search for SE Practice" 
+            id="search-input"
+          /> 
+          </div>
                <Dropdown/>
                <Styles>
                  <Table
