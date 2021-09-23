fe62877cf25643d6a0cac079618776282ee3b6aa
Travish <83637066+TravisPDC@users.noreply.github.com>
2021-09-23 15:25:21 +1200

    Adding Search box and filter functionality

--- seper-app/src/pages/SE-Practice.js
+++ seper-app/src/pages/SE-Practice.js
@@ -3,14 +3,13 @@ import Styles from "../components/tablestyle.js";
 import Table from "../components/evidencetable.js";
 import tablecolumns from "../components/tablecolumns.js";
 import Dropdown from "../components/Dropdown.js";
+import SearchBar from "../components/SearchBar.js";
 
 const SEPractice = () => {
     return (
       <div>
         <h2>Select SE Practice to get evidence for the claimed benefits</h2>
-        <div class="SEsearch-bar">
-          <input type="search" placeholder="Search for SE Practice" id="search-input"/>
-        </div>
+                <SearchBar placeholder="Enter the Title" data={articles}/>
                <Dropdown/>
                <Styles>
                  <Table
