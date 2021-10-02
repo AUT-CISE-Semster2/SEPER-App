125cf2cf51a6cb03324053e64a8a8fd9de53a212
Travishh <82255211+Travishh@users.noreply.github.com>
2021-10-02 20:21:27 +1300

    Attempt to connect backend to frontend

--- seper-app/src/components/evidencetable.js
+++ seper-app/src/components/evidencetable.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, {useMemo} from "react";
 //import articles from "../Data/articles.js";
 import { useTable, useSortBy, usePagination } from 'react-table';
--- seper-app/src/pages/SE-Practice.js
+++ seper-app/src/pages/SE-Practice.js
@@ -1,9 +1,10 @@
-import articles from "../Data/articles.js";
-import Styles from "../components/tablestyle.js";
-import Table from "../components/evidencetable.js";
-import tablecolumns from "../components/tablecolumns.js";
-import Dropdown from "../components/Dropdown.js";
+// import articles from "../Data/articles.js";
+// import Styles from "../components/tablestyle.js";
+// import Table from "../components/evidencetable.js";
+// import tablecolumns from "../components/tablecolumns.js";
+// import Dropdown from "../components/Dropdown.js";
 import React from "react";
+import axios from "axios";
 //import articles from "./routes/articles.js";
 
 
@@ -24,8 +25,62 @@ import React from "react";
 //     );
 // }
  
-// export default SEPractice;  
-
+const Article = props => (
+  <tr>
+    <td>{props.article.title}</td>
+  </tr>
+)
 class SEPractice extends React.Component{
+  // constructor(props){
+  //   super(props);
+  //   this.state = {articles: []};
+  // }
+  state ={articles: []};
+  componentDidMount() {
+    this.getArticle();
+  }
+
+  getArticle = () => {
+    axios.get('http://localhost:5000/articles/')
+      .then((res) => {
+        const data =res.data
+        this.setState({articles: data});
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  displayArticle = (articles) => {
+    // this.state.articles.map(currentArticle => {
+    //   return <Article article={currentArticle} key={currentArticle.id}/>;
+    // })
+    if(!articles.length) return null;
+    return articles.map((article, index) => {
+      <div key={index}>
+        <h3>{article.title}</h3>
+      </div>
+    });
+  };
+
+  render() {
+    return (
+            <div>
+              <h2>Select SE Practice to get evidence for the claimed benefits</h2>
+                     {/* <Dropdown
+                      title="SE Methods"             
+                     />
+                     <Styles>
+                       <Table
+                        data={articles}
+                        columns={tablecolumns}
+                       />
+                    </Styles> */}
+                    {this.displayArticle(this.state.articles)}
+            </div>
+          );
+  }
+
 
 }
+export default SEPractice;  
