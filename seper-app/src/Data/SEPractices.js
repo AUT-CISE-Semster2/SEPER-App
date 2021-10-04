ce28d5e569bfaa39a426b90129264f493a2d376c
reyhan2498 <reyhan2498@gmail.com>
2021-10-04 15:15:39 +1300

    Update SEPractices.js

--- seper-app/src/Data/SEPractices.js
+++ seper-app/src/Data/SEPractices.js
@@ -1,13 +1,16 @@
 const SEPractices = [
   {
     id: 1,
-    practice: 'TDD',
+    practice: 'All Methods',
   },
   {
     id: 2,
+    practice: 'TDD',
+  },
+  {
+    id: 3,
     practice: 'Mob programming',
   },
- 
 ];
 
 export default SEPractices;
