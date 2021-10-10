cffff93a591529dc10ac0c5a11581ed41d27b23d
reyhan2498 <reyhan2498@gmail.com>
2021-10-10 23:59:17 +1300

    changed the SE page

--- seper-app/src/pages/SE-Practice.js
+++ seper-app/src/pages/SE-Practice.js
@@ -1,12 +1,22 @@
 
 import React from "react";
 import ArticleList from "../components/ArticleList.js";
+import articles from "../Data/articles";
+import tablecolumns from "../components/tablecolumns.js";
+import Table from "../components/evidencetable.js";
+import Styles from "../components/tablestyle.js";
 
 const SEPractice = () => {
     return (
       <div>
         <h2>Select SE Practice to get evidence for the claimed benefits</h2>
         <h3>Select Method: </h3>
+        <Styles>
+          <Table
+            data={articles}
+            columns={tablecolumns}
+        />
+        </Styles>
             <ArticleList/>
       </div>
     );
